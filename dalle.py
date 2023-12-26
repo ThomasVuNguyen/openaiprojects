a8641308ef5d9091a307957c120bc377f53fe7d3
Thomas Nguyen <tungvunguyennguyen@gmail.com>
2023-12-25 22:43:20 -0500

    Updated for more customization

--- dalle.py
+++ dalle.py
@@ -1,21 +1,20 @@
 from openai import OpenAI
 from pathlib import Path
 import os
-
+import sys
 with open('API.txt') as file:
         api = file.read().replace('\n','')
 client = OpenAI(api_key=api)
+
 response = client.images.generate(
-        model = 'dall-e-2',
-        prompt = 'a minimal remote control robot application',
-        size = '256x256',
-        quality='standard',
-        n=1,
+        model =sys.argv[2],
+        prompt = sys.argv[1],
+        size = sys.argv[3],
+        quality= sys.argv[4],
+        n=int(sys.argv[5]),
 )
 
 image_url = response.data[0].url
 
 print(image_url)
-
-
 
